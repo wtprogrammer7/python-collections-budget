44e7fe82d9dace83c34805f7534d3a02e199742e
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-15 11:02:05 -0700

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -13,3 +13,6 @@ class BudgetList():
         else:
             self.overages.append(item)
             self.sum_overages += item
+
+    def __len__(self):
+        return self.expenses + len(self.overages)
