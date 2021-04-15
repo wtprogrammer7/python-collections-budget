318ce415539dcd52a09eac5d2b9da85702b64ca4
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-15 10:49:56 -0700

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -9,4 +9,4 @@ class BudgetList():
     def append(self,item):
         if self.sum_expenses + item < self.budget:
             self.expenses.append(item)
-            self.sum_expenses.append(item)
+            self.sum_expenses += item
