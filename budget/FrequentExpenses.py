e7bacaa9e93687035ff1297ce1641ddbe450ff3d
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-15 10:39:29 -0700

    Update FrequentExpenses.py

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import collections
 from . import Expense
 
@@ -13,3 +14,5 @@ spending_counter = collections.Counter(spending_categories)
 top5 = spending_counter.most_common(5)
 
 categories, count = zip(*top5)
+
+fig, ax = plt.subplots()
