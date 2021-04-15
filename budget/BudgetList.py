772a1c2b040a20a735d075074db91da2b098f57d
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-15 10:46:47 -0700

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,2 +1,7 @@
-class BudgetList:
-    pass
+class BudgetList():
+    def __init__(self, budget):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
