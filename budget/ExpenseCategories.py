c5c5323226baae6adb0ce78153c21b347d8f643d
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-16 10:14:58 -0700

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -6,6 +6,8 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
     divided_set_comp = expenses.categorize_set_comprehension()
+    if divided_set_comp != divided_for_loop:
+        print('Sets are NOT equal by == test')
 
 
 if __name__ == "__main__":
