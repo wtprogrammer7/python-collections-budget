04c638a1ba42382f5edd1377b5796cb656094a7c
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-16 09:45:30 -0700

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -46,6 +46,7 @@ def main():
 
     fig, ax = plt.subplots()
     labels = ['Expenses', 'Overages', 'Budget']
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
 
 if __name__ == "__main__":
     main()
