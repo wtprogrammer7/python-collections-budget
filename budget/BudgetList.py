e8d75df3cf2dbd8ba098de0b88f8983c7df9e6b2
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-16 09:33:34 -0700

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -20,7 +20,9 @@ class BudgetList():
         return len(self.expenses) + len(self.overages)
 
     def __iter__(self):
-        pass
+        self.iter = iter(self.expenses)
+        self.iter_o = iter(self.overages)
+        return self
 
 def main():
     myBudgetList = BudgetList(1200)
