a8d72aceb7f0e06786577a3e0f8261f9cc6333e4
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-16 10:29:52 -0700

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -12,8 +12,11 @@ def main():
     for a,b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)):
             print("Sets are NOT equal by subset test")
-    timeit.timeit(stmt = "pass", setup =
+    timeit.timeit(stmt = "expenses.categorize_for_loop()", setup =
     '''
+    from . import Expense
+    expenses = Expense.Expenses()
+    expenses.read_expenses('data/spending_data.csv')
     ''',
     number=100000, globals=globals()
     )
