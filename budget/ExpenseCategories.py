dabfc9e98a57c676a88aac5259d1e3ce0dc42071
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-16 10:13:30 -0700

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -5,6 +5,7 @@ def main():
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
+    divided_set_comp = expenses.categorize_set_comprehension()
 
 
 if __name__ == "__main__":
