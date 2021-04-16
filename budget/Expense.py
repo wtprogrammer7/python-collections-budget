243551b1e19a642bf8e27420caa271ed2908e434
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-16 10:07:49 -0700

    Update Expense.py

--- budget/Expense.py
+++ budget/Expense.py
@@ -42,4 +42,7 @@ class Expenses():
             return [necessary_expenses, food_expenses, unnecessary_expenses]
 
     def categorize_set_comprehension(self):
-        pass
+        necessary_expenses = {
+            x for x in self.list
+            if x.category == 'Phone' or x.category == 'Auto and Gas' or x.category == 'Classes' or x.category == 'Utilities' or x.category == 'Mortgage'
+        }
