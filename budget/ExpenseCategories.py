f39735deeda9fc40744b86bec534f7c666d9f0cd
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-16 10:33:45 -0700

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -23,5 +23,17 @@ expenses.read_expenses('data/spending_data.csv')
     number=100000,
     globals=globals()
     ))
+
+    print(timeit.timeit(stmt = "expenses.categorize_set_comprehension()",
+    setup =
+    '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+    ''',
+    number=100000,
+    globals=globals()
+    ))
+
 if __name__ == "__main__":
     main()
