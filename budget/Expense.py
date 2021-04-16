7468e3793443eeb3fe412be253604e49e8ca1bff
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-16 10:12:03 -0700

    Update Expense.py

--- budget/Expense.py
+++ budget/Expense.py
@@ -53,3 +53,5 @@ class Expenses():
         }
 
         unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
+
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
